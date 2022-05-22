2fbb31284be2eb634539f8923f8f9c02ec858993
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2022-05-22 11:29:33 +0300

    Add testing utils

---
+++ src/App.spec.tsx
@@ -0,0 +1,19 @@
+import App, { sum } from './App';
+import { render } from '@testing-library/react';
+
+describe('test for test', () => {
+  describe('App', () => {
+    it('should work', () => {
+      const component = render(<App />);
+
+      expect(component.queryByText('Hello Vite + React!')).toBeFalsy();
+      expect(component.queryByText('Perform project setup')).toBeTruthy();
+    });
+  });
+
+  describe('sum', () => {
+    it('should add two numbers', () => {
+      expect(sum(1, 2)).toBe(3);
+    });
+  });
+});
