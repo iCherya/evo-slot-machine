e6f3ae1c62e1a54ca794fa72140107c0929c8a7f
Vladyslav Cherednichenko <60984684+iCherya@users.noreply.github.com>
2022-05-23 22:22:38 +0300

    Project setup (#1)
    * Add testing utils
    
    * Add sass support
    
    * Add linter and formatter
    
    * Add pre-commit hook
    
    * Fix pre-commit hook
    
    * Add packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useCallback, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import logo from './logo.svg';
+
+import styles from './App.module.scss';
+
+export const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const onClickHandler = useCallback(() => {
+    setCount((c) => c + 1);
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <p>Perform project setup</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={onClickHandler}>
             count is: {count}
           </button>
         </p>
@@ -19,17 +25,12 @@ function App() {
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className={styles.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
           {' | '}
           <a
-            className="App-link"
+            className={styles.appLink}
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
@@ -39,7 +40,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export const sum = (a: number, b: number): number => a + b;
