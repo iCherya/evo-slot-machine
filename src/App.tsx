6d9ddf37ac426f6503735038ae2b17e40cc101b7
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2022-05-23 22:20:16 +0300

    Add packages

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ export const App: React.FC = () => {
   }, []);
 
   return (
-    <div className={styles.App}>
-      <header className={styles['App-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
         <p>Perform project setup</p>
         <p>
           <button type="button" onClick={onClickHandler}>
@@ -25,12 +25,12 @@ export const App: React.FC = () => {
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a className={styles['App-link']} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          <a className={styles.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
           {' | '}
           <a
-            className={styles['App-link']}
+            className={styles.appLink}
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
