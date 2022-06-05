8e65b792bce199f3bbd635be0a93d1f7d55a3896
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2022-06-06 01:14:46 +0300

    Revert Game component

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,33 +1,7 @@
-import React, { useCallback } from 'react';
-
-import { Button } from '@/components/shared/Button/Button';
-import { BUTTON_OPTIONS } from '@/config/buttons';
+import React from 'react';
 
 import styles from './Game.module.scss';
 
 export const Game: React.FC = () => {
-  const onButtonClickHandler = useCallback(() => {
-    console.log('Button clicked');
-  }, []);
-
-  return (
-    <div className={styles.wrapper}>
-      <p>Game component</p>
-      <Button size="small" type="primary" option="default" action={onButtonClickHandler}>
-        Primary
-      </Button>
-      <Button size="medium" type="secondary" action={onButtonClickHandler}>
-        Secondary
-      </Button>
-      <Button href="https://google.com" target="_blank">
-        Anchor
-      </Button>
-      <Button size="medium" type="disabled">
-        Disabled
-      </Button>
-      <Button type="github" url="#" size="meidum" option={BUTTON_OPTIONS.SOCIAL}>
-        Github
-      </Button>
-    </div>
-  );
+  return <div className={styles.wrapper}>Game component</div>;
 };
