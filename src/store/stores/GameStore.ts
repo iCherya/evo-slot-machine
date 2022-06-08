0195612468cddff7e83d9ccad5119ff567161283
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2022-06-09 00:04:38 +0300

    Add spinning status handling

--- src/store/stores/GameStore.ts
+++ src/store/stores/GameStore.ts
@@ -2,6 +2,8 @@ import { makeAutoObservable } from 'mobx';
 
 export class GameStore {
   private _isStarted = false;
+  private _isSpinning = false;
+  private _isSpinningEnded = true;
 
   public constructor() {
     makeAutoObservable(this);
@@ -11,7 +13,25 @@ export class GameStore {
     return this._isStarted;
   }
 
+  public get isSpinning(): boolean {
+    return this._isSpinning;
+  }
+
+  public get isSpinningEnded(): boolean {
+    return this._isSpinningEnded;
+  }
+
   public startGame(): void {
     this._isStarted = true;
+  }
+
+  public startSpinning(): void {
+    this._isSpinning = true;
+    this._isSpinningEnded = false;
+  }
+
+  public endSpinning(): void {
+    this._isSpinning = false;
+    this._isSpinningEnded = true;
   }
 }
--- src/store/stores/__tests__/GameStore.spec.ts
+++ src/store/stores/__tests__/GameStore.spec.ts
@@ -5,15 +5,37 @@ describe('GameStore', () => {
     expect(new GameStore()).toBeDefined();
   });
 
-  it('should have a property isStarted with initial false value', () => {
+  it('should have a property initial values', () => {
     const store = new GameStore();
+
     expect(store.isStarted).toBe(false);
+    expect(store.isSpinning).toBe(false);
+    expect(store.isSpinningEnded).toBe(true);
   });
 
   it('should have a method startGame that sets isStarted to true', () => {
     const store = new GameStore();
 
     store.startGame();
+
     expect(store.isStarted).toBe(true);
+  });
+
+  it('should have a method startSpinning that update spinning statuses', () => {
+    const store = new GameStore();
+
+    store.startSpinning();
+
+    expect(store.isSpinning).toBe(true);
+    expect(store.isSpinningEnded).toBe(false);
+  });
+
+  it('should have a method endSpinning that update spinning statuses', () => {
+    const store = new GameStore();
+
+    store.endSpinning();
+
+    expect(store.isSpinning).toBe(false);
+    expect(store.isSpinningEnded).toBe(true);
   });
 });
