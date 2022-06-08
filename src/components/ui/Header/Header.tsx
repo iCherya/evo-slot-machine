e85e6708be6cb20f86d439214cd52bef58d86ad3
Vladyslav Cherednichenko <cherednichenko.vlad@gmail.com>
2022-06-08 07:57:19 +0300

    Add AnimatedNumber

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import classNames from 'classnames';
 import { observer } from 'mobx-react-lite';
 
 import { useStore } from '@/store';
 import { CONTENT } from '@/config/content';
+import { AnimatedNumber } from '@/components/logic/AnimatedNumber';
 
 import styles from './Header.module.scss';
 
+const dummy = {
+  user: {
+    balance: 42,
+  },
+};
+
 export const Header: React.FC = observer(() => {
   const { app } = useStore();
+  const { user } = dummy;
+  const {
+    ui: { header },
+    domain: { currency },
+  } = CONTENT;
 
-  const { ui, domain } = CONTENT;
-  const userBalance = 0;
+  const format = useCallback((number: number) => `${Math.floor(number)} ${currency}`, [currency]);
 
   return (
     <header data-testid="header" className={classNames(styles.wrapper, app.isGameStarted && styles.game)}>
-      {!app.isGameStarted ? ui.header.title : `${userBalance} ${domain.currency}`}
+      {!app.isGameStarted ? header.title : <AnimatedNumber number={user.balance} format={format} />}
     </header>
   );
 });
